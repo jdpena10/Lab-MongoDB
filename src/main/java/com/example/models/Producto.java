fc9d8067eed3f4ff413c0040bb3f3037d1d09661
jdpena10 <juandaniel3478@gmail.com>
2024-04-29 10:38:55 -0500

    Se agregael proyecto

---
+++ src/main/java/com/example/models/Producto.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.models;
+
+import com.sun.istack.NotNull;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+
+@NoSql(dataFormat = DataFormatType.MAPPED)
+@Embeddable
+@XmlRootElement
+public class Producto {
+
+    private String name;
+    private String brand;
+    private int datePurchase;
+    @NotNull
+    @Column(name = "create_at", updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar createdAt;
+    @NotNull
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.DATE)
+    private Calendar updatedAt;
+
+    public Producto() {
+    }
+
+    @PreUpdate
+    private void updateTimestamp() {
+        this.updatedAt = Calendar.getInstance();
+    }
+
+    @PrePersist
+    private void creationTimestamp() {
+        this.createdAt = this.updatedAt = Calendar.getInstance();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getdatePurchase() {
+        return datePurchase;
+    }
+
+    public void setdatePurchase(int datePurchase) {
+        this.datePurchase = datePurchase;
+    }
+}
+
